e1dc139902f3d20eb6ee2db446d94305a7ccd449
Rahul Chowdhury <rahulc93@gmail.com>
2013-08-22 19:12:45 +0530

    Added the main.cpp

---
+++ helloworld/main.cpp
@@ -0,0 +1,14 @@
+// helloworld/main.cpp
+
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QLabel label("Hello World");
+    label.show();
+
+    return a.exec();
+}
